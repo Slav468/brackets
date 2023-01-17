5316adca666e0b56e61047a202d4ddff4710ee18
Slav468 <slav1988@tut.by>
2023-01-17 15:34:29 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+	const stack = [];
+	const group = {};
+	for (let i = 0; i < bracketsConfig.length; i++) {
+		group[bracketsConfig[i][0]] = bracketsConfig[i][1];
+	}
+	for (let i = 0; i < str.length; i++) {
+		if (group[stack[stack.length - 1]] == str[i]) {
+			stack.pop();
+		} else {
+			stack.push(str[i]);
+		}
+	}
+	return stack == 0;
+};
